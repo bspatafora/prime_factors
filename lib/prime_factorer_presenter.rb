6e4328b1e245bdcc5b9c4609266a18f38176c08f
bspatafora <benjamin.spatafora@gmail.com>
2014-06-18 20:47:42 -0500

    Minor presentational changes

--- lib/prime_factorer_presenter.rb
+++ lib/prime_factorer_presenter.rb
@@ -7,7 +7,8 @@ class PrimeFactorerPresenter
   def generate
     unless @number.nil?
       @number = @number.to_i
-      @prime_factorer.generate(@number)
+      prime_factors_array = @prime_factorer.generate(@number)
+      prime_factors_array.join(', ')
     end
   end
 end
