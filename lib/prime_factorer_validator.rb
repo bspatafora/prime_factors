390515c9dc986a04cda5e12afe81f6d58914ee13
bspatafora <benjamin.spatafora@gmail.com>
2014-06-24 21:53:35 -0500

    Refactor PrimeFactorerValidator

--- lib/prime_factorer_validator.rb
+++ lib/prime_factorer_validator.rb
@@ -6,11 +6,17 @@ class PrimeFactorerValidator
   ERROR_MESSAGE = 'Input is invalid'
 
   def error_message_if_invalid
-    return nil if @input.nil? || @input.empty?
+    return nil if no_input?(@input)
+
     number = Integer(@input)
     return ERROR_MESSAGE if number < 2
-    nil
     rescue ArgumentError
       ERROR_MESSAGE
+  end
+
+  private
+
+  def no_input?(input)
+    @input.nil? || @input.empty?
   end
 end
